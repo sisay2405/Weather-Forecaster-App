c2f0c4fe6acb257f339aefa36215ac359690051e
sisay2405 <abrehasisay04@gmail.comgit config --global user.name sisay2405egit config --global user.email abrehasisay04@gmail.com>
2022-07-13 13:34:05 -0400

    test searchbar

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,24 +1,25 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable comma-dangle */
 /* eslint-disable react/jsx-no-constructed-context-values */
 import React, { useState, useEffect, useMemo } from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
-import GlobalStyle from '../GlobalStyle';
+import GlobalStyle from '../globalStyle';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import ThemeContext from '../utils/ThemeContext';
 
-const Wrapper = styled.div`
-`;
+const Wrapper = styled.div``;
 
 const Layout = () => {
   const [theme, setTheme] = useState('weather');
   useEffect(() => {
-    document.title = `${
-      theme[0].toUpperCase() + theme.slice(1)
-    } App`;
+    document.title = `${theme[0].toUpperCase() + theme.slice(1)} App`;
   });
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'weather' ? 'seven-days-Weather' : 'weather'));
+    setTheme((prevTheme) =>
+      prevTheme === 'weather' ? 'seven-days-Weather' : 'weather',
+    );
   };
 
   return (
